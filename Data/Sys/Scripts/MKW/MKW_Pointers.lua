827a4f963a6d16616042f3408c3cb2b1a3d42f9e
TASLabz <96564043+TASLabz@users.noreply.github.com>
2022-05-14 14:21:31 -0400

    Revert "Malleoz/copy scripts to main repo"

--- Data/Sys/Scripts/MKW/MKW_Pointers.lua
+++ Data/Sys/Scripts/MKW/MKW_Pointers.lua
@@ -1,66 +1,22 @@
 local Pointers = {}
 
-local function getKMPBasePointer()
-  local kmpBase
-  if GetGameID() == "RMCP01" then kmpBase = 0x9BD6E8
-  elseif GetGameID() == "RMCE01"then kmpBase = 0x9B8F28
-  elseif GetGameID() == "RMCJ01" then kmpBase = 0x9BC748
-  elseif GetGameID() == "RMCK01" then kmpBase = 0x9ABD28
+local function getRaceData2Pointer(Offset)
+  local raceData2
+  if GetGameID() == "RMCP01" then raceData2 = 0x9BD730
+  elseif GetGameID() == "RMCE01"then raceData2 = 0x9B8F70
+  elseif GetGameID() == "RMCJ01" then raceData2 = 0x9BC790
+  elseif GetGameID() == "RMCK01" then raceData2 = 0x9ABD70
   end
-  return GetPointerNormal(kmpBase, 0x4, 0x0)
+  ReadValue32(pointer, 0xC, Offset)
 end
-Pointers.getKMPBasePointer = getKMPBasePointer
-
-local function getInputDataPointer()
-  local inputData
-  if GetGameID() == "RMCP01" then inputData = 0x9BD70C
-  elseif GetGameID() == "RMCE01"then inputData = 0x9B8F4C
-  elseif GetGameID() == "RMCJ01" then inputData = 0x9BC76C
-  elseif GetGameID() == "RMCK01" then inputData = 0x9ABD4C
-  end
-  return inputData
-end
-Pointers.getInputDataPointer = getInputDataPointer
-
-local function getRaceDataPointer()
-  local raceData
-  if GetGameID() == "RMCP01" then raceData = 0x9BD728
-  elseif GetGameID() == "RMCE01"then raceData = 0x9B8F68
-  elseif GetGameID() == "RMCJ01" then raceData = 0x9BC788
-  elseif GetGameID() == "RMCK01" then raceData = 0x9ABD68
-  end
-  return raceData
-end
-Pointers.getRaceDataPointer = getRaceDataPointer
-
-local function getRaceInfoPointer(Offset)
-  local raceInfo
-  if GetGameID() == "RMCP01" then raceInfo = 0x9BD730
-  elseif GetGameID() == "RMCE01"then raceInfo = 0x9B8F70
-  elseif GetGameID() == "RMCJ01" then raceInfo = 0x9BC790
-  elseif GetGameID() == "RMCK01" then raceInfo = 0x9ABD70
-  end
-  return GetPointerNormal(raceInfo, 0xC, Offset)
-end
-Pointers.getRaceInfoPointer = getRaceInfoPointer
+Pointers.getRaceData2Pointer = getRaceData2Pointer
 
 local function getInputPointer(Offset)
-  return ReadValue32(getRaceInfoPointer(Offset), 0x48, 0x4)
+  return ReadValue32(getRaceData2Pointer(Offset), 0x48, 0x4)
 end
 Pointers.getInputPointer = getInputPointer
 
-local function getRKSYSPointer()
-  local saveData
-  if GetGameID() == "RMCP01" then saveData = 0x9BD748
-  elseif GetGameID() == "RMCE01"then saveData = 0x9B8F88
-  elseif GetGameID() == "RMCJ01" then saveData = 0x9BC7A8
-  elseif GetGameID() == "RMCK01" then saveData = 0x9ABD88
-  end
-  return GetPointerNormal(saveData, 0x14, 0x0)
-end
-Pointers.getRKSYSPointer = getRKSYSPointer
-
-local function getPlayerPhysicsHolderPointer(Offset)
+local function getPrevPositionPointer(Offset)
   local pointer
   if GetGameID() == "RMCP01" then pointer = 0x9C18F8
   elseif GetGameID() == "RMCE01"then pointer = 0x9BD110
@@ -69,20 +25,20 @@ local function getPlayerPhysicsHolderPointer(Offset)
   end
   return GetPointerNormal(pointer, 0xC, 0x10, Offset, 0x0, 0x8, 0x90)
 end
-Pointers.getPlayerPhysicsHolderPointer = getPlayerPhysicsHolderPointer
+Pointers.getPrevPositionPointer = getPrevPositionPointer
 
-local function getPlayerStatsPointer(Offset)
+local function getPositionPointer(Offset)
   local pointer
   if GetGameID() == "RMCP01" then pointer = 0x9C18F8
   elseif GetGameID() == "RMCE01"then pointer = 0x9BD110
   elseif GetGameID() == "RMCJ01" then pointer = 0x9C0958
   elseif GetGameID() == "RMCK01" then pointer = 0x9AFF38
   end
-  return GetPointerNormal(pointer, 0x20, Offset, 0x0, 0x0, 0x14, 0x0)
+  return GetPointerNormal(pointer, 0xC, 0x10, Offset, 0x0, 0x8, 0x90, 0x4)
 end
-Pointers.getPlayerStatsPointer = getPlayerStatsPointer
+Pointers.getPositionPointer = getPositionPointer
 
-local function getPlayerSub10Pointer(Offset)
+local function getPlayerBasePointer(Offset)
   local pointer
   if GetGameID() == "RMCP01" then pointer = 0x9C18F8
   elseif GetGameID() == "RMCE01"then pointer = 0x9BD110
@@ -91,7 +47,7 @@ local function getPlayerSub10Pointer(Offset)
   end
   return ReadValue32(pointer, 0xC, 0x10, Offset, 0x10, 0x10)
 end
-Pointers.getPlayerSub10Pointer = getPlayerSub10Pointer
+Pointers.getPlayerBasePointer = getPlayerBasePointer
 
 local function getFrameOfInputAddress()
   local frameaddress
