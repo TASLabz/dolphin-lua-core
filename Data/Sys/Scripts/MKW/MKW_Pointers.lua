1b20972b304bdde192ff6c138fc2f6c8aafe2b21
vabold <vabold820@gmail.com>
2022-05-18 23:57:00 -0400

    Add new entries and restructure MKW_Pointers.lua
    This commit will break all Lua scripts, including MKW_Core.lua. Future commits will resolve these issues.

--- Data/Sys/Scripts/MKW/MKW_Pointers.lua
+++ Data/Sys/Scripts/MKW/MKW_Pointers.lua
@@ -6,86 +6,199 @@ function isViable()
   return false
 end
 
-local function getKMPBasePointer()
+-- global pointers
+local function getPlayerHolder()
   if not isViable() then return 0 end
-  ptrTable = { ["RMCP01"] = 0x9BD6E8, ["RMCE01"] = 0x9B8F28, ["RMCJ01"] = 0x9BC748, ["RMCK01"] = 0x9ABD28 }
-  local kmpBase =  ptrTable[GetGameID()]
-  return GetPointerNormal(pointer, 0x4, 0x0)
-end
-Pointers.getKMPBasePointer = getKMPBasePointer
-
-local function getInputDataPointer()
-  if not isViable() then return 0 end
-  ptrTable = { ["RMCP01"] = 0x9BD70C, ["RMCE01"] = 0x9B8F4C, ["RMCJ01"] = 0x9BC76C, ["RMCK01"] = 0x9ABD4C }
+  ptrTable = { ["RMCP01"] = 0x9C18F8, ["RMCE01"] = 0x9BD110, ["RMCJ01"] = 0x9C0958, ["RMCK01"] = 0x9AFF38 }
   return ptrTable[GetGameID()]
 end
-Pointers.getInputDataPointer = getInputDataPointer
+Pointers.getPlayerHolder = getPlayerHolder
 
-local function getRaceDataPointer()
+local function getRacedata()
   if not isViable() then return 0 end
   ptrTable = { ["RMCP01"] = 0x9BD728, ["RMCE01"] = 0x9B8F68, ["RMCJ01"] = 0x9BC788, ["RMCK01"] = 0x9ABD68 }
   return ptrTable[GetGameID()]
 end
-Pointers.getRaceDataPointer = getRaceDataPointer
+Pointers.getRacedata = getRacedata
 
-local function getRaceInfoPointer(Offset)
+local function getRaceinfo()
   if not isViable() then return 0 end
   ptrTable = { ["RMCP01"] = 0x9BD730, ["RMCE01"] = 0x9B8F70, ["RMCJ01"] = 0x9BC790, ["RMCK01"] = 0x9ABD70 }
-  local pointer = ptrTable[GetGameID()]
-  return GetPointerNormal(pointer, 0xC, Offset)
+  return ptrTable[GetGameID()]
 end
-Pointers.getRaceInfoPointer = getRaceInfoPointer
+Pointers.getRaceinfo = getRaceinfo
 
-local function getInputPointer(Offset)
-  return ReadValue32(getRaceInfoPointer(Offset), 0x48, 0x4)
+local function getKmpHolder()
+  if not isViable() then return 0 end
+  ptrTable = { ["RMCP01"] = 0x9BD6E8, ["RMCE01"] = 0x9B8F28, ["RMCJ01"] = 0x9BC748, ["RMCK01"] = 0x9ABD28 }
+  return ptrTable[GetGameID()]
 end
-Pointers.getInputPointer = getInputPointer
+Pointers.getKmpHolder = getKmpHolder
 
-local function getRKSYSPointer()
+local function getKclHolder()
+  if not isViable() then return 0 end
+  ptrTable = { ["RMCP01"] = 0x9C3C10, ["RMCE01"] = 0x9BF408, ["RMCJ01"] = 0x9C2C70, ["RMCK01"] = 0x9B2250 }
+  return ptrTable[GetGameID()]
+end
+Pointers.getKclHolder = getKclHolder
+
+local function getInputManager()
+  if not isViable() then return 0 end
+  ptrTable = { ["RMCP01"] = 0x9BD70C, ["RMCE01"] = 0x9B8F4C, ["RMCJ01"] = 0x9BC76C, ["RMCK01"] = 0x9ABD4C }
+  return ptrTable[GetGameID()]
+end
+Pointers.getInputManager = getInputManager
+
+local function getSaveManager()
   if not isViable() then return 0 end
   ptrTable = { ["RMCP01"] = 0x9BD748, ["RMCE01"] = 0x9B8F88, ["RMCJ01"] = 0x9BC7A8, ["RMCK01"] = 0x9ABD88 }
-  local pointer = ptrTable[GetGameID()]
-  return GetPointerNormal(pointer, 0x14, 0x0)
+  return ptrTable[GetGameID()]
 end
-Pointers.getRKSYSPointer = getRKSYSPointer
+Pointers.getSaveManager = getSaveManager
 
-local function getPlayerPhysicsHolderPointer(Offset)
-  if not isViable() then return 0 end
-  ptrTable = { ["RMCP01"] = 0x9C18F8, ["RMCE01"] = 0x9BD110, ["RMCJ01"] = 0x9C0958, ["RMCK01"] = 0x9AFF38 }
-  local pointer = ptrTable[GetGameID()]
-  return GetPointerNormal(pointer, 0xC, 0x10, Offset, 0x0, 0x8, 0x90)
-end
-Pointers.getPlayerPhysicsHolderPointer = getPlayerPhysicsHolderPointer
-
-local function getPlayerPhysicsPointer(Offset)
-  if not isViable() then return 0 end
-  ptrTable = { ["RMCP01"] = 0x9C18F8, ["RMCE01"] = 0x9BD110, ["RMCJ01"] = 0x9C0958, ["RMCK01"] = 0x9AFF38 }
-  local pointer = ptrTable[GetGameID()]
-  return GetPointerNormal(pointer, 0xC, 0x10, Offset, 0x0, 0x8, 0x90, 0x4)
-end
-Pointers.getPlayerPhysicsPointer = getPlayerPhysicsPointer
-
-local function getPlayerBasePointer(Offset)
-  if not isViable() then return 0 end
-  ptrTable = { ["RMCP01"] = 0x9C18F8, ["RMCE01"] = 0x9BD110, ["RMCJ01"] = 0x9C0958, ["RMCK01"] = 0x9AFF38 }
-  local pointer = ptrTable[GetGameID()]
-  return ReadValue32(pointer, 0xC, 0x10, Offset, 0x10, 0x10)
-end
-Pointers.getPlayerBasePointer = getPlayerBasePointer
-
-local function getPlayerStatsPointer(Offset)
-  if not isViable() then return 0 end
-  ptrTable = { ["RMCP01"] = 0x9C18F8, ["RMCE01"] = 0x9BD110, ["RMCJ01"] = 0x9C0958, ["RMCK01"] = 0x9AFF38 }
-  local pointer = ptrTable[GetGameID()]
-  return ReadValue32(pointer, 0x20, 0x0, Offset, 0x0, 0x0, 0x14, 0x0)
-end
-Pointers.getPlayerStatsPointer = getPlayerStatsPointer
-
-local function getFrameOfInputAddress()
+local function getFrameOfInput()
   if not isViable() then return 0 end
   ptrTable = { ["RMCP01"] = 0x9C38C0, ["RMCE01"] = 0x9BF0B8, ["RMCJ01"] = 0x9C2920, ["RMCK01"] = 0x9B1F00 }
   return ptrTable[GetGameID()]
 end
-Pointers.getFrameOfInputAddress = getFrameOfInputAddress
+Pointers.getFrameOfInput = getFrameOfInput
+
+-- scope of PlayerHolder
+local function getPlayer(playerIdx)
+  playerIdx = playerIdx or 0
+  return GetPointerNormal(getPlayerHolder(), 0x20, playerIdx * 0x4, 0x0)
+end
+Pointers.getPlayer = getPlayer
+
+local function getPlayerSub(playerIdx)
+  playerIdx = playerIdx or 0
+  return GetPointerNormal(getPlayer(playerIdx), 0x10, 0x0)
+end
+Pointers.getPlayerSub = getPlayerSub
+
+local function getPlayerSubClasses(playerIdx, offset)
+  playerIdx = playerIdx or 0
+  offset = offset or 10
+  return GetPointerNormal(getPlayerSub(playerIdx), offset, 0x0)
+end
+Pointers.getPlayerSubClasses = getPlayerSubClasses
+
+local function getPlayerParams(playerIdx)
+  playerIdx = playerIdx or 0
+  return GetPointerNormal(getPlayer(playerIdx), 0x0, 0x0, 0x0)
+end
+Pointers.getPlayerParams = getPlayerParams
+
+local function getPlayerStats(playerIdx)
+  playerIdx = playerIdx or 0
+  return GetPointerNormal(getPlayerParams(playerIdx), 0x14, 0x0, 0x0)
+end
+Pointers.getPlayerStats = getPlayerStats
+
+local function getPlayerHitboxes(playerIdx)
+  playerIdx = playerIdx or 0
+  return GetPointerNormal(getPlayerParams(playerIdx), 0x14, 0x4, 0x0)
+end
+Pointers.getPlayerHitboxes = getPlayerHitboxes
+
+local function getKartBody(playerIdx)
+  playerIdx = playerIdx or 0
+  return GetPointerNormal(getPlayer(playerIdx), 0x0, 0x8, 0x0)
+end
+Pointers.getKartBody = getKartBody
+
+local function getPlayerPhysicsHolder(playerIdx)
+  playerIdx = playerIdx or 0
+  return GetPointerNormal(getKartBody(playerIdx), 0x90, 0x0)
+end
+Pointers.getPlayerPhysicsHolder = getPlayerPhysicsHolder
+
+local function getPlayerPhysics(playerIdx)
+  playerIdx = playerIdx or 0
+  return GetPointerNormal(getPlayerPhysicsHolder(playerIdx), 0x4, 0x0)
+end
+Pointers.getPlayerPhysics = getPlayerPhysics
+
+local function getCollisionGroup(playerIdx)
+  playerIdx = playerIdx or 0
+  return GetPointerNormal(getPlayerPhysicsHolder(playerIdx), 0x8, 0x0)
+end
+Pointers.getCollisionGroup = getCollisionGroup
+
+local function getKartSus(playerIdx, wheelIdx)
+  playerIdx = playerIdx or 0
+  wheelIdx = wheelIdx or 0
+  return GetPointerNormal(getPlayer(playerIdx), 0x0, 0xC, wheelIdx * 0x4, 0x0)
+end
+Pointers.getKartSus = getKartSus
+
+local function getKartTire(playerIdx, wheelIdx)
+  playerIdx = playerIdx or 0
+  wheelIdx = wheelIdx or 0
+  return GetPointerNormal(getPlayer(playerIdx), 0x0, 0x10, wheelIdx * 0x4, 0x0)
+end
+Pointers.getKartTire = getKartTire
+
+local function getPlayerModel(playerIdx)
+  playerIdx = playerIdx or 0
+  return GetPointerNormal(getPlayer(playerIdx), 0x0, 0x14, 0x0)
+end
+Pointers.getPlayerModel = getPlayerModel
+
+-- scope of Racedata
+local function getRacedataScenario(scenarioIdx)
+  scenarioIdx = scenarioIdx or 0
+  return GetPointerNormal(getRacedata(), 0x20 + (0xBF0 * scenarioIdx), 0x0)
+end
+Pointers.getRacedataScenario = getRacedataScenario
+
+local function getRacedataPlayer(playerIdx, scenarioIdx)
+  scenarioIdx = scenarioIdx or 0
+  playerIdx = playerIdx or 0
+  return GetPointerNormal(getRacedataScenario(scenarioIdx), 0x8 + (0xF0 * playerIdx), 0x0)
+end
+Pointers.getRacedataPlayer = getRacedataPlayer
+
+local function getRacedataSettings(scenarioIdx)
+  scenarioIdx = scenarioIdx or 0
+  return GetPointerNormal(getRacedataScenario(scenarioIdx), 0xB48, 0x0)
+end
+Pointers.getRacedataSettings = getRacedataSettings
+
+-- scope of Raceinfo
+local function getRaceinfoPlayer(playerIdx)
+  playerIdx = playerIdx or 0
+  return GetPointerNormal(getRaceinfo(), 0xC, playerIdx, 0x0)
+end
+Pointers.getRaceinfoPlayer = getRaceinfoPlayer
+
+local function getController(playerIdx)
+  playerIdx = playerIdx or 0
+  return GetPointerNormal(getRaceinfoPlayer(playerIdx), 0x48, 0x4, 0x0)
+end
+Pointers.getController = getController
+
+local function getTimerManager()
+  return GetPointerNormal(getRaceinfo(), 0x14, 0x0)
+end
+Pointers.getTimerManager = getTimerManager
+
+-- scope of KmpHolder
+local function getRawKmpFile()
+  return GetPointerNormal(getKmpHolder(), 0x4, 0x0, 0x0)
+end
+Pointers.getRawKmpFile = getRawKmpFile
+
+-- scope of KclHolder
+local function getKclInfo()
+  return GetPointerNormal(getKclHolder(), 0x0, 0x0)
+end
+Pointers.getKclInfo = getKclInfo
+
+-- scope of SaveManager
+local function getRawSavePointer()
+  return GetPointerNormal(getSaveManager(), 0x14, 0x0)
+end
+Pointers.getRawSave = getRawSave
 
 return Pointers
